20d82f7bf6ab619f0fb6932d6a50cf318e16b816
Nathami-n <georgenathan010@gmail.com>
2024-05-19 11:19:42 +0300

    Added shadcn-ui form

--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -2,7 +2,13 @@ import { CardWrapper } from "@/components/auth/CardWrapper";
 
 const RegisterForm = () => {
     return (
-        <CardWrapper>
+        <CardWrapper
+        headerLabel="Create An account"
+        backButtonHref="/auth/login"
+        backButtonLabel="Already have an account? Login now"
+        showSocial
+        >
+            <div>Hello</div>
 
         </CardWrapper>
     )
