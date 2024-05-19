8a9eb944834e13f0ead2d89fa31f5719ccd23f2c
Nathami-n <georgenathan010@gmail.com>
2024-05-19 12:38:39 +0300

    Added the form skeleton

--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -1,10 +1,25 @@
+'use client';
 import { CardWrapper } from "@/components/auth/CardWrapper";
-import {Form}  from "@/components/ui/form";
+import {
+    Form,
+    FormField,
+    FormLabel,
+    FormControl,
+    FormItem,
+    FormMessage
+
+}  from "@/components/ui/form";
 import {useForm} from 'react-hook-form';
 import {RegisterSchema} from '@/schema';
+import {useTransition, useState} from 'react';
+import {z} from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod';
+import { Input } from "@/components/ui/input";
 
 const RegisterForm = () => {
+    const [isPending, startTransition] = useTransition();
+    const [error, setError] = useState<string | undefined>('');
+    const [success, setSuccess] = useState<string |undefined>('');
     const form = useForm({
         resolver: zodResolver(RegisterSchema),
         defaultValues: {
@@ -13,6 +28,9 @@ const RegisterForm = () => {
             name:""
         }
     });
+    const onSubmit = async (data: z.infer<typeof RegisterSchema>) => {
+
+    }
     return (
         <CardWrapper
         headerLabel="Create An account"
@@ -20,8 +38,33 @@ const RegisterForm = () => {
         backButtonLabel="Already have an account? Login now"
         showSocial
         >
-            <Form>
-
+            <Form {...form}>
+                <form
+                onSubmit = {form.handleSubmit(onSubmit)}
+                >
+                    <div>
+                        <FormField
+                        control={form.control}
+                        name="name"
+                        render={({field})=> (
+                            <FormItem>
+                                <FormLabel>
+                                    Name
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                    disabled={isPending}
+                                    {...field}
+                                    type="text"
+                                    placeholder="John Doe"
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                        />
+                    </div>
+                </form>
             </Form>
 
         </CardWrapper>
