0605740716fa8240bc4d0c35ae83bfb5a188ad0d
Nathami-n <georgenathan010@gmail.com>
2024-05-21 18:38:52 +0300

    Added dummy table data

--- components/dashboard/card/card.tsx
+++ components/dashboard/card/card.tsx
@@ -3,12 +3,12 @@ import { MdSupervisedUserCircle } from "react-icons/md";
 
 const Card = () => {
     return (
-        <div className="soft p-5 flex rounded-md gap-5 cursor-pointer">
+        <div className="soft p-5 flex rounded-md gap-5 cursor-pointer w-full hover:bg-[#2e374a] transition-all">
             <MdSupervisedUserCircle size={24}/>
             <div className="flex flex-col gap-5">
                 <span className="">Total Users</span>
                 <span className="text-[24px] font-semibold">50.345</span>
-                <span className="text-[14px] font-[300px]"><span>60%</span> more than last month</span>
+                <span className="text-[14px] font-[300px]"><span className="text-lime-500">60%</span> more than last month</span>
             </div>
         </div>
     )
--- components/dashboard/transactions/transactions.tsx
+++ components/dashboard/transactions/transactions.tsx
@@ -1,9 +1,100 @@
+import Image from "next/image"
 
 
 const Transactions = () => {
-  return (
-    <div>Transactions</div>
-  )
+    return (
+        <div className="p-5 soft rounded-md">
+            <h2 className="mb-5 font-[200]">Latest Dealings</h2>
+            <table className="w-full">
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Status</td>
+                        <td>Date</td>
+                        <td>Amount</td>
+                    </tr>
+                </thead>
+                <tbody className="gap-y-4">
+                    <tr>
+                        <td>
+                           <div className="flex gap-2 items-center">
+                           <Image
+                            src="/avatar.jpeg"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className="rounded-full object-cover"
+                        />
+                        Nathan George
+                           </div>
+                        </td>
+                        <td>
+                            <span>Pending</span>
+                        </td>
+                        <td>13.5.2003</td>
+                        <td>$600</td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <div className="flex gap-2 items-center">
+                           <Image
+                            src="/avatar.jpeg"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className="rounded-full object-cover"
+                        />
+                        Nathan George
+                           </div>
+                        </td>
+                        <td>
+                            <span>Pending</span>
+                        </td>
+                        <td>13.5.2003</td>
+                        <td>$600</td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <div className="flex gap-2 items-center">
+                           <Image
+                            src="/avatar.jpeg"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className="rounded-full object-cover"
+                        />
+                        Nathan George
+                           </div>
+                        </td>
+                        <td>
+                            <span>Done</span>
+                        </td>
+                        <td>13.5.2003</td>
+                        <td>$600</td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <div className="flex gap-2 items-center">
+                           <Image
+                            src="/avatar.jpeg"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className="rounded-full object-cover"
+                        />
+                        Nathan George
+                           </div>
+                        </td>
+                        <td>
+                            <span>Cancelled</span>
+                        </td>
+                        <td>13.5.2003</td>
+                        <td>$600</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Transactions
